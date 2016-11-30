7fbee78839eb27949465fac4abcc2cbe6e9f301f
CanSenturk <b141210043@sakarya.edu.tr>
2016-11-30 21:42:28 +0200

    Can degıstırdı

---
+++ dergiproje/dergiproje/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dergiproje.Startup))]
+namespace dergiproje
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
